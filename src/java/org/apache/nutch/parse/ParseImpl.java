1b9ee336ab20e79fc608fff72b924efc295dbc58
zceolrj <zceolrj@gmail.com>
2014-12-31 12:26:06 +0800

    commit at dormitory

--- src/java/org/apache/nutch/parse/ParseImpl.java
+++ src/java/org/apache/nutch/parse/ParseImpl.java
@@ -26,55 +26,71 @@ import org.apache.hadoop.io.*;
  */
 public class ParseImpl implements Parse, Writable 
 {
-  private ParseText text;
-  private ParseData data;
-  private boolean isCanonical;
+	private ParseText text;
+	private ParseData data;
+	private boolean isCanonical;
 
-  public ParseImpl() {}
+	public ParseImpl() {}
 
-  public ParseImpl(Parse parse) {
-    this(new ParseText(parse.getText()), parse.getData(), true);
-  }
+	public ParseImpl(Parse parse) 
+	{
+		this(new ParseText(parse.getText()), parse.getData(), true);
+	}
 
-  public ParseImpl(String text, ParseData data) {
-    this(new ParseText(text), data, true);
-  }
+	public ParseImpl(String text, ParseData data) 
+	{
+		this(new ParseText(text), data, true);
+	}
   
-  public ParseImpl(ParseText text, ParseData data) {
-    this(text, data, true);
-  }
+	public ParseImpl(ParseText text, ParseData data) 
+	{
+		this(text, data, true);
+	}
 
-  public ParseImpl(ParseText text, ParseData data, boolean isCanonical) {
-    this.text = text;
-    this.data = data;
-    this.isCanonical = isCanonical;
-  }
+	public ParseImpl(ParseText text, ParseData data, boolean isCanonical) 
+	{
+	    this.text = text;
+	    this.data = data;
+	    this.isCanonical = isCanonical;
+	}
 
-  public String getText() { return text.getText(); }
+	public String getText() 
+	{ 
+		return text.getText(); 
+	}
 
-  public ParseData getData() { return data; }
+	public ParseData getData() 
+	{ 
+		return data; 
+	}
 
-  public boolean isCanonical() { return isCanonical; }
+	public boolean isCanonical() 
+	{ 
+		return isCanonical; 
+	}
   
-  public final void write(DataOutput out) throws IOException {
-    out.writeBoolean(isCanonical);
-    text.write(out);
-    data.write(out);
-  }
+	public final void write(DataOutput out) throws IOException 
+	{
+	    out.writeBoolean(isCanonical);
+	    text.write(out);
+	    data.write(out);
+	}
 
-  public void readFields(DataInput in) throws IOException {
-    isCanonical = in.readBoolean();
-    text = new ParseText();
-    text.readFields(in);
+	public void readFields(DataInput in) throws IOException 
+	{
+	    isCanonical = in.readBoolean();
+	    text = new ParseText();
+	    text.readFields(in);
+	
+	    data = new ParseData();
+	    data.readFields(in);
+	}
 
-    data = new ParseData();
-    data.readFields(in);
-  }
-
-  public static ParseImpl read(DataInput in) throws IOException {
-    ParseImpl parseImpl = new ParseImpl();
-    parseImpl.readFields(in);
-    return parseImpl;
-  }
+	public static ParseImpl read(DataInput in) throws IOException 
+	{
+	    ParseImpl parseImpl = new ParseImpl();
+	    parseImpl.readFields(in);
+	    return parseImpl;
+	}
 
 }
